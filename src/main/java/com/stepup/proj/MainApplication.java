914a49edfe8d97da5438ca3dfd8d0cff73ab77b3
Nataly Doroshenko <git.user.kras_red@mail.ru>
2025-04-24 17:03:46 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,5 +4,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hellow World");
         System.out.println("11111");
+        System.out.println("2222");
     }
 }
